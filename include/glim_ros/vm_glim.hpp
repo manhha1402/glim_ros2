f06ffd5fccf32bb5af3b33c4e65739a3bd35d7a1
Ha Hoang <v.hahm@vinmotion.net>
2026-01-29 13:58:45 +0700

    added vm_glim node

---
+++ include/glim_ros/vm_glim.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <any>
+#include <deque>
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+
+#include <glim_ros/vm_glim_parameters.hpp>
+
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#ifdef BUILD_WITH_CV_BRIDGE
+#include <image_transport/image_transport.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#endif
+
+namespace glim {
+class TimeKeeper;
+class CloudPreprocessor;
+class AsyncOdometryEstimation;
+class AsyncSubMapping;
+class AsyncGlobalMapping;
+
+class ExtensionModule;
+class GenericTopicSubscription;
+
+class VMGlim : public rclcpp::Node {
+public:
+  VMGlim(const rclcpp::NodeOptions& options);
+  ~VMGlim();
+
+  bool needs_wait();
+  void timer_callback();
+
+  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+#ifdef BUILD_WITH_CV_BRIDGE
+  void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr msg);
+#endif
+  size_t points_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg);
+
+  void wait(bool auto_quit = false);
+  void save(const std::string& path);
+
+  const std::vector<std::shared_ptr<GenericTopicSubscription>>& extension_subscriptions();
+
+private:
+  std::unique_ptr<glim::TimeKeeper> time_keeper;
+  std::unique_ptr<glim::CloudPreprocessor> preprocessor;
+
+  std::shared_ptr<glim::AsyncOdometryEstimation> odometry_estimation;
+  std::unique_ptr<glim::AsyncSubMapping> sub_mapping;
+  std::unique_ptr<glim::AsyncGlobalMapping> global_mapping;
+
+  bool keep_raw_points;
+  double imu_time_offset;
+  double points_time_offset;
+  double acc_scale;
+  bool dump_on_unload;
+
+  std::shared_ptr<vm_glim::ParamListener> param_listener_;
+  vm_glim::Params params_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  void params_callback();
+  std::string intensity_field, ring_field;
+
+  // Extension modulles
+  std::vector<std::shared_ptr<ExtensionModule>> extension_modules;
+  std::vector<std::shared_ptr<GenericTopicSubscription>> extension_subs;
+
+  // ROS-related
+  rclcpp::TimerBase::SharedPtr timer;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr points_sub;
+#ifdef BUILD_WITH_CV_BRIDGE
+  image_transport::Subscriber image_sub;
+#endif
+};
+
+}  // namespace glim
